76e5559f500a9ee7ce0314fa59840a70604079ce
authorbeard <msnolan@gmail.com>
2016-11-13 17:55:39 -0500

    began adding data labels, gridlines

--- js/chartMaker.js
+++ js/chartMaker.js
@@ -1,6 +1,7 @@
 
 function drawChart(dayArray){
-  var raw = dayArray;
+
+  var raw = dayArray.reverse();
 //Setting variables to be used throughout
     var fillParse=d3.timeFormat("%m%d%y")
     var opParse=d3.timeFormat("%U")
@@ -13,6 +14,11 @@ function drawChart(dayArray){
     var margin = {top: 20, right: 20, bottom: 40, left: 50},
         height = 500 - margin.top - margin.bottom,
         width = 960 - margin.left - margin.right;        
+    
+    var fillSelector = {
+          "0": "#cecfd0",
+          "1": "#1224d8"
+        }
 
   raw.forEach(function (e, i, arry){
     var data=e.data;
@@ -25,18 +31,38 @@ function drawChart(dayArray){
     var yScale = d3.scaleLinear()
       .domain(d3.extent(data, function(d){return d.entrances}))
       .rangeRound([height, 0]);
-      
+    
+    var x_gridlines = make_x_gridlines(xScale);
+    chart.append("g")     
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .call(x_gridlines
+          .tickSize(-height)
+          .tickFormat("")
+      )
+
+    // make_y_gridlines(yScale);
+    //   svg.append("g")     
+    //   .attr("class", "grid")
+    //   .call(make_y_gridlines()
+    //       .tickSize(-width)
+    //       .tickFormat("")
+    //   )
+
+//begin drawing/filling lines for each day    
+
     data.forEach(function(d){
       d.time = new Date(d.time)
     })
 
-//Only draw axes based on first day of data
     if (i==0){
       g.append("g")
           .attr("class", "axis x")
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(xScale))
         .append("text")
+          .attr("transform", "translate(" + width/2 + ", 50)")
+          .attr("style", "font-size: 18")
           .text("Time");
 
       g.append("g")
@@ -48,17 +74,15 @@ function drawChart(dayArray){
           .attr("y", -40)
           .attr("x", (height/2)*-1)
           .style("text-anchor", "middle")
+          .attr("style", "font-size: 18")
           .text("Entrances");
     }
 
   //Complete lines and fill areas for each day
-debugger;
     var line = d3.line()
       .curve(d3.curveBasis)
       .x(function(d) { return xScale(d.time); })
       .y(function(d) { return yScale(d.entrances); });
-
-
 
     var area = d3.area()
         .curve(d3.curveBasis)
@@ -75,8 +99,29 @@ debugger;
         .data([data])
         .attr("class", "area")
         .attr("d", area)
-        .attr("opacity", ".5")
-        .attr("style", "fill: #" + fillParse(data[0].time));
+        .attr("style", "opacity: " + (i/3 + .2) + "; fill: " + fillSelector[i]);
+// debugger;
+    g.append("rect")
+        .attr("transform", "translate(" + (width-100) + "," + i*margin.top + ")")
+        .attr("style", "opacity: " + (i/3 + .2) + "; fill: " + fillSelector[i])
+        .attr("width", "10")
+        .attr("height", "10")
+      .append("text")
+        .attr("transform", "translate(" + (width-margin.right) + "," + i*margin.top + ")")
+        .attr("style", "fill: " + fillSelector[i])
+        .text(e.name.toString());
 
   })
 }
+
+function make_x_gridlines(scale) {   
+    return d3.axisBottom(scale)
+        .ticks(5)
+}
+
+function make_y_gridlines(scale) {   
+    return d3.axisLeft(scale)
+        .ticks(5)
+}
+
+
